3715dbadbbc03dd0492951905926e33ddcb2a514
Alumno <ulsa@iMacD112.local>
2019-01-16 18:42:23 -0700

    Nice contactos

--- app/Http/Controllers/InicioController.php
+++ app/Http/Controllers/InicioController.php
@@ -3,10 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Contacto;
 
 class InicioController extends Controller
 {
     public function hola () {
-        return view('hola');
+        //Obtengo TODOS los registros de la tabla contactos
+        $contactos = Contacto::all();
+
+        //Creando argumentos con la informacion que va a utilizar la vista
+        $argumentos = array();
+        $argumentos['contactos'] = $contactos;
+
+        return view('hola', $argumentos);
     }
 }
--- resources/views/hola.blade.php
+++ resources/views/hola.blade.php
@@ -10,6 +10,34 @@
 </head>
 <body>
     <h1> Hola! </h1>
-    <p> Usted es un goven?</p>
+    <p> Estas personas son govenes: </p>
+
+    <table>
+        <thead>
+            <tr>
+                <th>Nombre</th>
+                <th></th>
+                <th></th>
+                <th>Telefono</th>
+                <th></th>
+                <th></th>
+                <th>Correo</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($contactos as $contacto)
+                <tr>
+                    <td>{{ $contacto->nombre }}</td>
+                    <td></td>
+                    <td></td>
+                    <td>{{ $contacto->telefono }}</td>
+                    <td></td>
+                    <td></td>
+                    <td>{{ $contacto->correo }}</td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+
 </body>
 </html>
